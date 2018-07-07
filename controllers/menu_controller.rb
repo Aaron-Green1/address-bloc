ea6416488d1ae2a045c99777fb3911da58eaa32c
Aaron M Green <txhc010888@gmail.com>
2018-07-07 12:16:28 -0500

    cp5 assignment - no merge with master

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View an entry by number"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your Selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_entry_by_num
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good bye!"
         exit(0)
       else 
@@ -56,6 +61,20 @@ class MenuController
 
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry_by_num
+    print "Please enter an entry number (enter -1 to go back to main menu): "
+    selection = gets.chomp.to_i
+    
+    if selection == -1
+      main_menu
+    elsif selection <= address_book.entries.length && selection > 0
+      puts address_book.entries[selection - 1]
+    else
+      puts "Invalid entry"
+      view_entry_by_num
+    end
   end
 
   def create_entry
