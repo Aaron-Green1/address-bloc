694404eba8bc6540773d8268d10ac298a607bb50
Aaron M Green <txhc010888@gmail.com>
2018-07-07 17:14:29 -0500

    ruby cp6 CSVs assignment

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -91,4 +91,31 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#import_from_csv_2" do
+    it "imports the correct number of entries from 2nd CSV file" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry from the 2nd CSV file" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Aaron", "555-555-1888", "aaron@blocmail.com")
+    end
+
+    it "imports the 2nd entry from the 2nd CSV file" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Hawkeye", "555-555-0525", "hawkeye@blocmail.com")
+    end
+
+    it "imports the 3rd entry from the 2nd CSV file" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Kenny", "555-555-0620", "kenny@blocmail.com")
+    end
+  end
+
 end
